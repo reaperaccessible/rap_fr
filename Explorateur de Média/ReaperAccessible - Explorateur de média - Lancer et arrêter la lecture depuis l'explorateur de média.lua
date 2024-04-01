d57c4bd859e6459887dffb41aae539d6a133b201
Ludovic SANSONE <reaperaccessible@gmail.com>
2024-04-01 19:35:00 +0200

    Ajout de tous les scripts de l'explorateur de média

---
+++ Explorateur de Média/ReaperAccessible - Explorateur de média - Lancer et arrêter la lecture depuis l'explorateur de média.lua
@@ -0,0 +1,7 @@
+-- @description Lancer et arrêter la lecture depuis l'explorateur de média
+-- @version 1.0
+-- @author Ludovic SANSONE pour Reaper Accessible
+-- @provides [main=mediaexplorer] .
+
+
+reaper.Main_OnCommand(40044, 0)
