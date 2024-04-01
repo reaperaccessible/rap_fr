bd38df03eeecc025c28cc16aee2305d43fe80f5c
Ludovic SANSONE <reaperaccessible@gmail.com>
2024-04-01 18:44:10 +0200

    Ajout de l'attribut provides au nouveau script

--- Explorateur de Média/ReaperAccessible - Explorateur de média - Aller au marqueur précédent.lua
+++ Explorateur de Média/ReaperAccessible - Explorateur de média - Aller au marqueur précédent.lua
@@ -1,6 +1,6 @@
 -- @description déplace le curseur au marqueur précédent
 -- @version 1.0
 -- @author Ludovic SANSONE pour Reaper Accessible
-
+-- @provides [main=mediaexplorer] .
 
 reaper.Main_OnCommand(40172, 0)
