b58ad365b7eb561e93bf5d692ffde3344d61871a
Ludovic SANSONE <libreaudio63@gmail.com>
2024-05-03 18:20:27 +0200

    MAJ de l'index

---
+++ create-index.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+path = os.getcwd()
+repository_name = os.path.basename(path)
+
+if os.getcwd() == "rap_fr":
+    repository_name = "ReaperAccessible scripts"
+elif os.getcwd() == "rap_en":
+    repository_name = "ReaperAccessible scripts US"
+
+script = f"""git add -A & \
+git rm index.xml & \
+reapack-index -w -n {repository_name} --no-commit & \
+git add index.xml & \
+git commit -m \"MAJ de l'index\" & \
+git push
+"""
+
+subprocess.run(script, shell=True)
