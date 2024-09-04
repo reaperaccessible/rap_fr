eed053318d4d1f57898aaf9ab13c1e5c11bb31c5
leejul <90565835+reaperaccessible@users.noreply.github.com>
2024-09-04 11:12:05 -0400

    Mise à jour de l'index

--- Principal/ReaperAccessible - Principal - Énoncer le volume de la piste sélectionnée.lua
+++ Principal/ReaperAccessible - Principal - Énoncer le volume de la piste sélectionnée.lua
@@ -1,30 +1,32 @@
 -- @description Énonce le volume de la piste sélectionnée
--- @version 1.2
+-- @version 1.3
 -- @author Lee JULIEN pour ReaperAccessible. 
 -- @provides [main=main] .
 
 
--- Vérifie si le projet contient des pistes
-local countTracks = reaper.CountTracks(0)
-if countTracks == 0 then
-    reaper.osara_outputMessage("No tracks in your project")
-    return
+-- Récupère la piste sélectionnée
+local selected_track = reaper.GetSelectedTrack(0, 0)
+
+if selected_track then
+    -- Récupère le volume de la piste sélectionnée
+    local selected_track_volume = reaper.GetMediaTrackInfo_Value(selected_track, "D_VOL")
+    
+    -- Convertit le volume en décibels
+    local selected_track_volume_db = 20 * math.log(selected_track_volume) / math.log(10)
+    
+    -- Énonce uniquement la valeur du volume en décibels
+    reaper.osara_outputMessage(string.format("%.2f", selected_track_volume_db) .. " dB")
+else
+    -- Vérifie si la piste master est sélectionnée
+    local master_track = reaper.GetMasterTrack(0)
+    if master_track then
+        -- Récupère le volume de la piste master
+        local master_track_volume = reaper.GetMediaTrackInfo_Value(master_track, "D_VOL")
+        
+        -- Convertit le volume en décibels
+        local master_track_volume_db = 20 * math.log(master_track_volume) / math.log(10)
+        
+        -- Énonce uniquement la valeur du volume en décibels
+        reaper.osara_outputMessage(string.format("%.2f", master_track_volume_db) .. " dB")
+    end
 end
-
--- Obtient la première piste sélectionnée
-local track = reaper.GetSelectedTrack2(0, 0, 1)
-
--- Vérifie si une piste est sélectionnée
-if not track then
-    reaper.osara_outputMessage("No track selected")
-    return
-end
-
--- Obtient le volume de la piste
-local volume = 20 * math.log(reaper.GetMediaTrackInfo_Value(track, "D_VOL"), 10)
-
--- Convertir le volume en chaîne
-local s_volume = tostring(volume)
-
--- Énonce le volume de la piste
-reaper.osara_outputMessage(s_volume)
--- Principal/ReaperAccessible - Principal - Énoncer le volume du Master.lua
+++ Principal/ReaperAccessible - Principal - Énoncer le volume du Master.lua
@@ -1,5 +1,5 @@
 -- @description Énoncer le volume du Master
--- @version 1.0
+-- @version 1.1
 -- @author Lee JULIEN for Reaper Accessible
 -- @provides [main=main] .
 
@@ -7,36 +7,14 @@
 -- Récupère la piste Master
 local masterTrack = reaper.GetMasterTrack(0)
 
--- Récupère le volume de la piste Master en dB
-local masterVolume = 20 * math.log(reaper.GetMediaTrackInfo_Value(masterTrack, "D_VOL"), 10)
+-- Récupère le volume de la piste Master
+local masterVolume = reaper.GetMediaTrackInfo_Value(masterTrack, "D_VOL")
 
--- Convertit le volume en chaîne de caractères
-local s_masterVolume = tostring(masterVolume)
+-- Convertit le volume en dB
+local masterVolumeDB = 20 * math.log(masterVolume, 10)
 
--- Initialise les variables pour le formatage de la chaîne
-local i = 0
-local len = s_masterVolume:len()
-
--- Choix de la valeur de 'i' en fonction de la valeur du volume
-if masterVolume >= 10 then
-    i = 5
-elseif masterVolume > 0 then
-    i = 4
-elseif masterVolume <= -100 then
-    i = 7
-elseif masterVolume <= -10 then
-    i = 6
-else
-    i = 5
-end
-
--- Ajoute un '0' à la fin de la chaîne si nécessaire
-if len < i then
-    s_masterVolume = s_masterVolume .. '0'
-end
-
--- Extrait la sous-chaîne à énoncer en fonction de 'i'
-s_masterVolume = s_masterVolume:sub(0, i)
+-- Arrondit la valeur à deux décimales
+local roundedVolume = string.format("%.2f", masterVolumeDB)
 
 -- Énonce le volume de la piste Master
-reaper.osara_outputMessage("Master Volume" .. s_masterVolume)
+reaper.osara_outputMessage(roundedVolume .. " dB")
