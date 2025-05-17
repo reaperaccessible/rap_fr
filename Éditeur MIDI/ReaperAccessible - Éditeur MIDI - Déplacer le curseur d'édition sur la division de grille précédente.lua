3718f55654425505f115bff0c8c478a54472994f
Lee <90565835+reaperaccessible@users.noreply.github.com>
2025-05-17 19:57:10 -0400

    Mise à jour de l'index

--- Éditeur MIDI/ReaperAccessible - Éditeur MIDI - Déplacer le curseur d'édition sur la division de grille précédente.lua
+++
@@ -1,9 +0,0 @@
--- @description Déplacer le curseur d'édition sur la division de grille précédente
--- @version 1.0
--- @author Lee JULIEN pour ReaperAccessible
--- @provides [main=midi_editor] .
--- @changelog
---   # 2025-05-17 - Nouveau script
-
-
-reaper.Main_OnCommand(40646, 0)
